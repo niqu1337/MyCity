dad0a44485f5eedd84f33dccab1e3301bb8007f8
niqu1337 <61757012+niqu1337@users.noreply.github.com>
2021-01-28 18:35:07 +0100

    Minimal changes in src

--- MyCity.cmd
+++ MyCity.cmd
@@ -60,179 +60,178 @@ goto startup
     goto error
 
 :HireMode
-	cls
-	echo 1 - Hire Police man
-	echo 2 - Hire Firefighter
-	echo 3 - Hire Paramedic
-	echo 4 - Hire Emergency call operator
-	echo 5 - Exit hire mode
-	set /p SelectHire= 
-	if %SelectHire%==1 goto HirePolice
-	if %SelectHire%==2 goto HireFire
-	if %SelectHire%==3 goto HireMedic
-	if %SelectHire%==4 goto HireOperator
-	if %SelectHire%==5 goto game
-	goto error
+    cls
+    echo 1 - Hire Police man
+    echo 2 - Hire Firefighter
+    echo 3 - Hire Paramedic
+    echo 4 - Hire Emergency call operator
+    echo 5 - Exit hire mode
+    set /p SelectHire= 
+    if %SelectHire%==1 goto HirePolice
+    if %SelectHire%==2 goto HireFire
+    if %SelectHire%==3 goto HireMedic
+    if %SelectHire%==4 goto HireOperator
+    if %SelectHire%==5 goto game
+    goto error
 
 :WorkMode
-	cls
-	echo 1 - Work in Mc Donald's
-	echo 2 - Rob ATM
-	echo 3 - Work as 911 Operator
-	echo 4 - Exit work mode
-	set /p SelectWork= 
-	if %SelectWork%==1 goto MCDonalds
-	if %SelectWork%==2 goto ATM
-	if %SelectWork%==2 goto Operator
-	if %SelectWork%==4 goto game
-	goto error
+    cls
+    echo 1 - Work in Mc Donald's
+    echo 2 - Rob ATM
+    echo 3 - Work as 911 Operator
+    echo 4 - Exit work mode
+    set /p SelectWork= 
+    if %SelectWork%==1 goto MCDonalds
+    if %SelectWork%==2 goto ATM
+    if %SelectWork%==2 goto Operator
+    if %SelectWork%==4 goto game
+    goto error
 
 
 
 :BuildHouse
-	cls
-	echo Building house... (wait 3 sec)
-	ping -n 3 localhost > nul
-	set /a cash=cash-housePrice
-	set /a countH=countH+1
-	set /a citizens=citizens+4
-	echo House Builded!
-	echo Your houses %countH%
-	echo Your cash %cash%
-	goto game
+    cls
+    echo Building house... (wait 3 sec)
+    ping -n 3 localhost > nul
+    set /a cash=cash-housePrice
+    set /a countH=countH+1
+    set /a citizens=citizens+4
+    echo House Builded!
+    echo Your houses %countH%
+    echo Your cash %cash%
+    goto game
 
 :BuildApartment
-	cls
-	echo Building apartment... (wait 5 sec)
-	ping -n 5 localhost > nul
-	set /a cash=cash-apartmentPrice
-	set /a countA=countA+1
-	set /a citizens=citizens+28
-	echo Apartment Builded!
-	echo Your apartments %countH%
-	echo Your cash %cash%
-	goto game
+    cls
+    echo Building apartment... (wait 5 sec)
+    ping -n 5 localhost > nul
+    set /a cash=cash-apartmentPrice
+    set /a countA=countA+1
+    set /a citizens=citizens+28
+    echo Apartment Builded!
+    echo Your apartments %countH%
+    echo Your cash %cash%
+    goto game
 
 :BuildVilla
-	cls
-	echo Building villa... (wait 8 sec)
-	ping -n 8 localhost > nul
-	set /a cash=cash-villaPrice
-	set /a countV=countV+1
-	set /a citizens=citizens+58
-	echo Villa Builded!
-	echo Your villas %countH%
-	echo Your cash %cash%
-	goto game
+    cls
+    echo Building villa... (wait 8 sec)
+    ping -n 8 localhost > nul
+    set /a cash=cash-villaPrice
+    set /a countV=countV+1
+    set /a citizens=citizens+58
+    echo Villa Builded!
+    echo Your villas %countH%
+    echo Your cash %cash%
+    goto game
 
 
 
 :HirePolice
-	cls
-	set /a timeout=(%random% %%18)
-	if timeout==0 goto HirePolice
-	echo Finding police man... (wait %timeout% sec)
-	ping -n %timeout% localhost > nul
-	
-	set /a citizens=citizens+1
-	echo Finded new police man!
-	ping -n 3 localhost > nul
-	goto game
+    cls
+    set /a timeout=(%random% %%18)
+    if timeout==0 goto HirePolice
+    echo Finding police man... (wait %timeout% sec)
+    ping -n %timeout% localhost > nul
+    
+    set /a citizens=citizens+1
+    echo Finded new police man!
+    ping -n 3 localhost > nul
+    goto game
 
 :HireFire
-	cls
-	set /a timeout=(%random% %%24)
-	if timeout==0 goto HireFire
-	echo Finding firefighter... (wait %timeout% sec)
-	ping -n %timeout% localhost > nul
-	
-	set /a citizens=citizens+1
-	echo Finded new firefighter!
-	ping -n 3 localhost > nul
-	goto game
+    cls
+    set /a timeout=(%random% %%24)
+    if timeout==0 goto HireFire
+    echo Finding firefighter... (wait %timeout% sec)
+    ping -n %timeout% localhost > nul
+    
+    set /a citizens=citizens+1
+    echo Finded new firefighter!
+    ping -n 3 localhost > nul
+    goto game
 
 :HireMedic
-	cls
-	set /a timeout=(%random% %%28)
-	if timeout==0 goto HireMedic
-	echo Finding paramedic... (wait %timeout% sec)
-	ping -n %timeout% localhost > nul
-	
-	set /a citizens=citizens+1
-	echo Finded new paramedic!
-	ping -n 3 localhost > nul
-	goto game
+    cls
+    set /a timeout=(%random% %%28)
+    if timeout==0 goto HireMedic
+    echo Finding paramedic... (wait %timeout% sec)
+    ping -n %timeout% localhost > nul
+    
+    set /a citizens=citizens+1
+    echo Finded new paramedic!
+    ping -n 3 localhost > nul
+    goto game
 
 :HireOperator
-	cls
-	set /a timeout=(%random% %%30)
-	if timeout==0 goto HireOperator
-	echo Finding operator... (wait %timeout% sec)
-	ping -n %timeout% localhost > nul
-	
-	set /a citizens=citizens+1
-	echo Finded new operator!
-	ping -n 3 localhost > nul
-	goto game
+    cls
+    set /a timeout=(%random% %%30)
+    if timeout==0 goto HireOperator
+    echo Finding operator... (wait %timeout% sec)
+    ping -n %timeout% localhost > nul
+    
+    set /a citizens=citizens+1
+    echo Finded new operator!
+    ping -n 3 localhost > nul
+    goto game
 
 
 
 :MCDonalds
-	cls
-	set /a timeout=13
-	if timeout==0 goto McDonalds
-	echo Working in McDonalds... (wait %timeout% sec)
-	ping -n %timeout% localhost > nul
-	
-	set /a earnedCash=(%random% %%80)
-	if earnedCash==0 goto MCDonalds
-	set /a cash=cash+earnedCash
-	echo You earned %earnedCash%!
-	ping -n 3 localhost > nul
-	goto game
+    cls
+    set /a timeout=13
+    if timeout==0 goto McDonalds
+    echo Working in McDonalds... (wait %timeout% sec)
+    ping -n %timeout% localhost > nul
+    
+    set /a earnedCash=(%random% %%80)
+    if earnedCash==0 goto MCDonalds
+    set /a cash=cash+earnedCash
+    echo You earned %earnedCash%!
+    ping -n 3 localhost > nul
+    goto game
 
 :Operator
-	cls
-	echo In MyCity-v3.2.1!
-	timeout 10 >>nul
-	goto game
+    cls
+    echo In MyCity-v3.2.1!
+    timeout 10 >>nul
+    goto game
 
 :ATM
-	cls
-	set /a timeout=18
-	set /a var1=var1+1
-	echo Robbing atm... (wait %timeout% sec)
-	ping -n %timeout% localhost > nul
-	
-	set /a roll=(%random% %%2)
-	if roll==0 goto ATM
-	if roll==1 goto ATMOK
-	if roll==2 goto ATMBAD
-
-		:ATMOK
-		set /a earnedCash=(%random% %%300)
-		if earnedCash==0 goto ATMOK
-		set /a cash=cash+earnedCash
-		echo You earned %earnedCash%!
-		ping -n 3 localhost > nul
-		goto game
-		
-		:ATMBAD
-		set /a lostCash=(%random% %%600)
-		if lostCash==0 goto ATMBAD
-		set /a cash=cash+lostCash
-		echo You lost %lostCash%!
-		ping -n 3 localhost > nul
-		goto game
-		
-
-
+    cls
+    set /a timeout=18
+    set /a var1=var1+1
+    echo Robbing atm... (wait %timeout% sec)
+    ping -n %timeout% localhost > nul
+    
+    set /a roll=(%random% %%2)
+    if roll==0 goto ATM
+    if roll==1 goto ATMOK
+    if roll==2 goto ATMBAD
+        
+        :ATMOK
+        set /a earnedCash=(%random% %%300)
+        if earnedCash==0 goto ATMOK
+        set /a cash=cash+earnedCash
+        echo You earned %earnedCash%!
+        ping -n 3 localhost > nul
+        goto game
+        
+        :ATMBAD
+        set /a lostCash=(%random% %%600)
+        if lostCash==0 goto ATMBAD
+        set /a cash=cash+lostCash
+        echo You lost %lostCash%!
+        ping -n 3 localhost > nul
+        goto game
+        
+        
 :LottoMode
-	cls
-	echo In MyCity-v3.2!
-	timeout 10 >>nul
-	goto game
-
+    cls
+    echo In MyCity-v3.2!
+    timeout 10 >>nul
+    goto game
+	
 :CreateSave
 	cls
 	echo Enter your player name
